17eb1b9112f321d4cb88a218ad4c39933c400dc4
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-10-13 15:41:13 -0400

    [WIP] UV Index color code and icons on 5 Day Forecast

--- script.js
+++ script.js
@@ -27,9 +27,53 @@ $(document).ready(function () {
     }).then(function (response) {
       console.log(response);
       //   console.log(queryURL);
-      $("#temperature").text("Temperature: " + response.main.temp + " ℉");
-      $("#humidity").text("Humidity: " + response.main.humidity + "%");
-      $("#windspeed").text("Wind Speed: " + response.wind.speed + " MPH");
+      var mainDate = moment().format("L");
+      var cityNameEl = $("<h2>").text(response.name);
+      var displayMainDate = cityNameEl.append(" " + mainDate);
+      var tempEl = $("<p>").text("Temperature: " + response.main.temp + " ℉");
+      var humidityEl = $("<p>").text(
+        "Humidity: " + response.main.humidity + "%"
+      );
+      var windEl = $("<p>").text("Wind Speed: " + response.wind.speed + " MPH");
+
+      var currentWeather = response.weather[0].main;
+
+      if (currentWeather === "Rain") {
+        var currentIcon = $("<img>").attr(
+          "src",
+          "http://openweathermap.org/img/wn/09d.png"
+        );
+        currentIcon.attr("style", "height: 60px; width: 60px");
+      } else if (currentWeather === "Clouds") {
+        var currentIcon = $("<img>").attr(
+          "src",
+          "http://openweathermap.org/img/wn/03d.png"
+        );
+        currentIcon.attr("style", "height: 60px; width: 60px");
+      } else if (currentWeather === "Clear") {
+        var currentIcon = $("<img>").attr(
+          "src",
+          "http://openweathermap.org/img/wn/01d.png"
+        );
+        currentIcon.attr("style", "height: 60px; width: 60px");
+      } else if (currentWeather === "Drizzle") {
+        var currentIcon = $("<img>").attr(
+          "src",
+          "http://openweathermap.org/img/wn/10d.png"
+        );
+        currentIcon.attr("style", "height: 60px; width: 60px");
+      } else if (currentWeather === "Snow") {
+        var currentIcon = $("<img>").attr(
+          "src",
+          "http://openweathermap.org/img/wn/13d.png"
+        );
+        currentIcon.attr("style", "height: 60px; width: 60px");
+      }
+
+      var newDiv = $("<div>");
+      newDiv.append(displayMainDate, currentIcon, tempEl, humidityEl, windEl);
+
+      $("#current").html(newDiv);
 
       var lat = response.coord.lat;
       var lon = response.coord.lon;
@@ -46,10 +90,10 @@ $(document).ready(function () {
         method: "GET",
       }).then(function (response) {
         console.log(response);
-        console.log(queryURLUV);
+        // console.log(queryURLUV);
         $("#uvindex-display").empty();
         var uviResults = response.value;
-        var uviEl = $("<div></div>").text("UV Index: " + response.value);
+        var uviEl = $("<button class='btn bg-danger'>").text("UV Index: " + uviResults);
         $("#uvindex-display").html(uviEl);
       });
     });
@@ -68,7 +112,7 @@ $(document).ready(function () {
         );
 
         var date = results[i].dt_txt;
-        var setDate = date.substr(0, 10);
+        var setDate = new Date(results[i].dt * 1000).toLocaleDateString();
         var temp = results[i].main.temp;
         var hum = results[i].main.humidity;
         console.log(temp);
@@ -79,38 +123,38 @@ $(document).ready(function () {
         fiveDayDiv.append(h5Date);
 
         if (weather === "Rain") {
-            var icon = $("<img>").attr(
-              "src",
-              "http://openweathermap.org/img/wn/09d.png"
-            );
-            icon.attr("style", "height: 40px; width: 40px");
-          } else if (weather === "Clouds") {
-            var icon = $("<img>").attr(
-              "src",
-              "http://openweathermap.org/img/wn/03d.png"
-            );
-          } else if (weather === "Clear") {
-            icon.attr("style", "height: 40px; width: 40px");
-          } else if (weather === "Clear") {
-            var icon = $("<img>").attr(
-              "src",
-              "http://openweathermap.org/img/wn/01d.png"
-            );
-            icon.attr("style", "height: 40px; width: 40px");
-          } else if (weather === "Drizzle") {
-            var icon = $("<img>").attr(
-              "src",
-              "http://openweathermap.org/img/wn/10d.png"
-            );
-            icon.attr("style", "height: 40px; width: 40px");
-          } else if (weather === "Snow") {
-            var icon = $("<img class=>").attr(
-              "src",
-              "http://openweathermap.org/img/wn/13d.png"
-            );
-            icon.attr("style", "height: 40px; width: 40px");
-          }
-          fiveDayDiv.append(icon);
+          var icon = $("<img>").attr(
+            "src",
+            "http://openweathermap.org/img/wn/09d.png"
+          );
+          icon.attr("style", "height: 40px; width: 40px");
+        } else if (weather === "Clouds") {
+          var icon = $("<img>").attr(
+            "src",
+            "http://openweathermap.org/img/wn/03d.png"
+          );
+        } else if (weather === "Clear") {
+          icon.attr("style", "height: 40px; width: 40px");
+        } else if (weather === "Clear") {
+          var icon = $("<img>").attr(
+            "src",
+            "http://openweathermap.org/img/wn/01d.png"
+          );
+          icon.attr("style", "height: 40px; width: 40px");
+        } else if (weather === "Drizzle") {
+          var icon = $("<img>").attr(
+            "src",
+            "http://openweathermap.org/img/wn/10d.png"
+          );
+          icon.attr("style", "height: 40px; width: 40px");
+        } else if (weather === "Snow") {
+          var icon = $("<img class=>").attr(
+            "src",
+            "http://openweathermap.org/img/wn/13d.png"
+          );
+          icon.attr("style", "height: 40px; width: 40px");
+        }
+        fiveDayDiv.append(icon);
 
         var pTemp = $("<p class='card-text'>").text("Temp: " + temp + " ℉");
         fiveDayDiv.append(pTemp);
@@ -119,8 +163,6 @@ $(document).ready(function () {
         var weather = results[i].weather[0].main;
 
         $("#5day").append(fiveDayDiv);
-
-        
       }
     });
   }
